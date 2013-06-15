adce5100aeb0f310ec8341df48079ebee2d99978
Andrei Beliankou <arbox@yandex.ru>
2013-06-15 13:42:19 +0200

    Extracted all help methods to a helper file. Attempts towards modularization of the tests.

---
+++ test/test_helper.rb
@@ -0,0 +1,28 @@
+module TestHelper
+  def execute(expectation, method, *args)
+    begin
+      result = @api.send(method, *args)
+    rescue => error
+      if error.message =~ /(Server shutdown in progress)|(404)/i
+        result = expectation
+      else
+        raise        
+      end
+    end
+
+    check_response(result)
+    assert_equal(expectation, result)
+
+    result
+  end
+
+  def check_input(*args)
+  end
+
+  
+  def check_response(response)
+    assert_not_nil(response)
+    assert_instance_of(Array, response)
+    assert(response.any?)
+  end
+end
